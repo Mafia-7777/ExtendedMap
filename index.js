0857a6645d52453dbe243abab41c37da34f2ac66
Mafia-boss-lvl-420 <67177667+Mafia-boss-lvl-420@users.noreply.github.com>
2020-12-01 02:36:34 -0500

    :open_mouth:

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ module.exports = class ExtendedMap extends Map {
         if(!filter) new Error("A filter is required");
         let newMap = new ExtendedMap();
         for(const keys of this){
-            if(filter({value: keys[0], key: keys[1]})) newMap.set(keys[0], keys[1]);
+            console.log(keys)
+            if(filter({value: keys[1], key: keys[0]})) newMap.set(keys[0], keys[1]);
         }
         return newMap;
     }
